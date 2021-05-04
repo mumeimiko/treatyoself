816ea62ee1fe03309b172fd2d2e257174b4dbc50
mumeimiko <79484291+mumeimiko@users.noreply.github.com>
2021-05-03 23:51:18 -0500

    Update node_check.py

--- project3/node_check.py
+++ project3/node_check.py
@@ -1,64 +1,57 @@
-#Author Mumeimiko Email@michael.zng@yahoo.com
+#Author Mumeimiko Email@ mumeimiko@yahoo.com
 #tl:dr check kubernetes nodes from k8s and marks them as unhealthy and delete them
 #v.1.1.0
 from kubernetes import client, config
 import json, array, boto3
 
+#8-13 Getting stuff ready 
 client2 = boto3.client('autoscaling')
-# Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
 bad_nodes = []
 good_nodes = []
 v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
 ret = v1.list_node(watch=False)
+
+#For loop that looks at each node 
 for i in ret.items:
     
     node = i.metadata.name
-    print( f'Node in question: {i.metadata.name} ')
+    print( f'EKS Node in question: {i.metadata.name} ')
     print( f'EC2 in question: {i.spec.provider_id} ')
     if "fargate" in i.spec.provider_id:
-        print("This is a fargate node")
+        print("This is a fargate node, ignoring")
     else:
-        print("this is an ec2 instance")
+        print("EC2 Node")
+        #Locates the instance id
         ec2_node = i.spec.provider_id.split("/", 4)
         node = ec2_node[4]
+    #29-32 Grab the Node Status from an EKS Standpoint
     test = i.status.conditions[-1:]
-    #print( test )
     test2=str(test)
     status = (test2.splitlines()[4])
     type = (test2.splitlines()[5])
-    #print (status)
-    #print (type)
+    #24-39 Will append the node to the Bad/Good Node List
     if "True" in status:
         print ('Node Currently is in a Ready Status')
         good_nodes.append(node)
     else:
         print ('Node Currently is having issues')
         bad_nodes.append(node)
-    #print (i)
+#41-45 print the current nodes
 print("Currently the good nodes are:")
 print (good_nodes)
 
 print("Currently the bad nodes are:")
 print (bad_nodes)
-#print (bad_nodes[0])
-response = client2.set_instance_health(
-    HealthStatus='Unhealthy',
-    InstanceId=bad_nodes[0],
-    ShouldRespectGracePeriod=False
-)
 
-print(response)
-""" test2=str(test)
-    print (test2)
-    lastline = test2[-5]
-    print (lastline)"""
-
-
-
-#cordon https://www.programcreek.com/python/example/96328/kubernetes.client.CoreV1Api
-
-#
-#
-#
+#48-57 Marking the nodes as unhealthy via the ASG
+if len(bad_nodes) == 0:
+    print("All nodes are marked as Ready")
+else:
+    for i in len(bad_nodes):
+        client2.set_instance_health(
+            HealthStatus='Unhealthy',
+            InstanceId=bad_nodes[i],
+            ShouldRespectGracePeriod=False
+        )
+        print("Terminating: " + bad_nodes[i] )
