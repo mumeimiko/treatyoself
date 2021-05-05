bb4b6166e299b3d722c5433978a7d3c8d2e33d51
TotallyNotAPrinter <renteddi@amazon.com>
2021-05-05 13:59:35 -0700

    pep8 and typo fix

--- project3/node_check.py
+++ project3/node_check.py
@@ -1,21 +1,21 @@
-#v.1.1.2
+# v.1.1.2
 from kubernetes import client, config
 
-import json 
-import array 
-import boto3 
+import json
+import array
+import boto3
 import re
-import pprint
 
-# Need to drop some globs once the logic is fully tested 
+# Need to drop some globs
 ec2 = boto3.client('ec2')
 kubecfg = config.load_kube_config()
 k8s_api = client.CoreV1Api()
 list_node = k8s_api.list_node(watch=False, _preload_content=False)
 http_decode = list_node.data.decode('utf8')
-pp = pprint.PrettyPrinter(indent=4)
 
-# 18-39 cleaned up eval logic, will now ignore fargate nodes in the top for loop 
+# 18-39 cleaned up eval logic, will now ignore fg nodes
+
+
 def node_status(http_decode):
     unhealthy_nodes = []
     healthy_nodes = []
@@ -31,23 +31,37 @@ def node_status(http_decode):
             instance_id = provider_id[4]
             status = i['status']['conditions'][3]
             if status['status'] == 'True':
-                node_info = {'instance_id': instance_id, "health_status": status['reason']}
+                node_info = {
+                    'instance_id': instance_id,
+                    "health_status": status['reason']
+                }
                 healthy_nodes.append(node_info)
             else:
-                node_info = {'instance_id': instance_id, "health_status": status['reason']}
+                node_info = {
+                    'instance_id': instance_id,
+                    "health_status": status['reason']
+                }
                 unhealthy_nodes.append(node_info)
-    determine_health(unhealthy_nodes, healthy_nodes)
+    determine_health(
+        unhealthy_nodes,
+        healthy_nodes
+    )
 
-# 41-48 prints statuses for healthy/unhealthy nodes, makes the call to term here
+# 41-48 determines what node to terminate
+
+
 def determine_health(unhealthy_nodes, healthy_nodes):
+    uh_nodes_count = len(unhealthy_nodes)
     print(f'These nodes are currently healthy: {healthy_nodes}')
-    if len(unhealthy_nodes) != 0:
-        print(f'there is {len(unhealthy_nodes)} unhealthy nodes, terminating them')
+    if uh_nodes_count != 0:
+        print(f'there is {uh_nodes_count} unhealthy nodes, terminating them')
         ec2_asg_call(unhealthy_nodes)
     else:
-        print(f'there are {len(unhealthy_nodes)} unhealthy nodes, nothing to do')
+        print(f'there are {uh_nodes_count} unhealthy nodes, nothing to do')
 
-# 51-62 this is only called when there is unhealthy nodes, terms in a batch 
+# 51-62 this is only called when there is unhealthy nodes, terms in a batch
+
+
 def ec2_asg_call(unhealthy_nodes):
     instance_ids = []
     for i in unhealthy_nodes:
@@ -57,7 +71,7 @@ def ec2_asg_call(unhealthy_nodes):
         InstanceIds=instance_ids,
         DryRun=False
     )
-    print(f"Terminating: {instance_ids}" )
+    print(f"Terminating: {instance_ids}")
 
 if __name__ == "__main__":
     node_status(http_decode)
--- project3/readme.md
+++ project3/readme.md
@@ -8,6 +8,6 @@ This script does the following, in order:
 5. for ec2 nodes, it pulls the instanceID from the providerID key nested within `spec` 
 6. for the same node object, it will then determine the status of the node and append it to a relevant list
 7. once the lists are fully popluated and we've iterated across all of the nodes, it makes a call to a function to evaluate health
-8. this function starts by printing healthy nodes, and if the lenght of the unhealthy nodes array isn't equal to 0 it calls a function to terminate the instances 
+8. this function starts by printing healthy nodes, and if the length of the unhealthy nodes array isn't equal to 0 it calls a function to terminate the instances 
 9. the function to terminate is only called when there is unhealthy instances, instead of making 1 api call for each instance in the list, I batched them and instead call the `terminate_instances` method so that we can terminate the unhealthy nodes in one go (this reduces the number of API calls made)
 
